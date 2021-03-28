eadf2180ce75a1ff3b57daed2f92bd24bf0734f8
IamHerica <hericacadoni@gmail.com>
2021-03-28 00:02:49 -0300

    mudando o nome dos arquivos

---
+++ brincando com uma lampada/script.js
@@ -0,0 +1,7 @@
+function ligar() {
+    document.getElementById("img").src = "img/lamp_on.gif";
+}
+
+function desligar() {
+    document.getElementById("img").src = "img/lamp_off.gif";
+}
---
+++ calculadora/script.js
@@ -0,0 +1,10 @@
+function Calcular() {
+
+ var operacao = e.value;
+ 
+ var n1 = document.getElementById("Numero1").value;
+ var n2 = document.getElementById("Numero2").value;
+
+ if (document.getElementById).value("calculadora") == "Soma"
+
+}
