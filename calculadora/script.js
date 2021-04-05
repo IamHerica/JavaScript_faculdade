4428c2afc7f5e538f37ea39b3e1f64159a73adef
Herica Cadoni <71198776+IamHerica@users.noreply.github.com>
2021-04-05 19:02:28 -0300

    Update script.js

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,10 +1,22 @@
-function Calcular() {
+function calculo () {
+  var n1 = document.getElementById("caixa1").value;
+  var n2 = document.getElementById("caixa2").value;
 
- var operacao = e.value;
- 
- var n1 = document.getElementById("Numero1").value;
- var n2 = document.getElementById("Numero2").value;
+  if (document.getElementById("operacao").value == "Soma") {
+    var n3 = parseInt(n1) + parseInt(n2);
+  }
 
- if (document.getElementById).value("calculadora") == "Soma"
+   if (document.getElementById("operacao").value == "Subtrair") {
+    var n3 = parseInt(n1) - parseInt(n2);
+  }
 
+   if (document.getElementById("operacao").value == "Multiplicar") {
+    var n3 = parseInt(n1) * parseInt(n2);
+  }
+
+   if (document.getElementById("operacao").value == "Dividir") {
+    var n3 = parseInt(n1) / parseInt(n2);
+  }
+
+  alert("O resultado de sua operação é: "+ n3)
 }
